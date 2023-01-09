46f99f5f576a4852a58502d9cae73248143eee7f
Anton Zalialdinov <zloomsky@gmail.com>
2023-01-09 20:09:38 +0200

    feat: some rafactor

--- src/ExportParser.Alfa/Program.cs
+++ src/ExportParser.Alfa/Program.cs
@@ -12,6 +12,8 @@ namespace ExportParser.Alfa
 {
     public static class Program
     {
+        public static string _pageHeader = "Дата проводки Код операции Описание Сумма  в валюте счета";
+
         public static Regex _alfaLine = new Regex(@"(?<sum>-[0-9 ]+,\d{2} RUR)");
 
         public static Regex _dateLine = new Regex(@"(?<date>\d{2}\.\d{2}\.\d{2,4}),");
@@ -35,17 +37,10 @@ namespace ExportParser.Alfa
             var assembly = typeof(Program).GetTypeInfo().Assembly;
             var resourceName = assembly.GetManifestResourceNames().First(s => s.EndsWith(embeddedFileName, StringComparison.CurrentCultureIgnoreCase));
 
-            using (var stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                if (stream == null)
-                {
-                    throw new InvalidOperationException("Could not load manifest resource stream.");
-                }
-                using (var reader = new StreamReader(stream))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+         
+            return reader.ReadToEnd();
         }
 
         public static string MatchOr(this Regex first, Regex second, string value)
@@ -57,6 +52,17 @@ namespace ExportParser.Alfa
             }
 
             return second.Match(value).Groups["date"].Value;
+        }
+
+        public static string TrimStartByText(this string input, string text)
+        {
+            var splitInput = input.Split(text);
+            if (splitInput.Length == 1)
+            {
+                return input;
+            }
+
+            return string.Join(text, splitInput.Skip(1));
         }
        
         public static string ReadText(string filePath)
@@ -72,6 +78,16 @@ namespace ExportParser.Alfa
             return stringBuilder.ToString();
         }
 
+        public static string[] ReadAllLines(string rawText)
+        {
+            var lines = _alfaLine.Split(rawText);
+            return lines
+                .Select((x, i) => $"{x}{lines.ElementAtOrDefault(i + 1)}")
+                .Where((_, i) => i % 2 == 0)
+                .Select(r => r.TrimStartByText(_pageHeader))
+                .ToArray();
+        }
+
         public static TValue TryGet<TKey, TValue>(this Dictionary<TKey, TValue> source, TKey key) => source.TryGetValue(key, out TValue value) ? value : default(TValue);
 
         public static DateTime? TryDate(string source) => DateTime.TryParse(source, out DateTime date) ? date : new DateTime?();
@@ -80,13 +96,11 @@ namespace ExportParser.Alfa
 
         static void Main(string[] args)
         {
-            var inputPdf = new FileInfo("D:\\trash\\document30.09.22 20_23_31.488.pdf");
+            var inputPdf = new FileInfo("D:\\учет\\document09.01.23 191216.654.pdf");
             var allText = ReadText(inputPdf.FullName);
-            var lines = _alfaLine.Split(allText).ToArray();
+            var lines = ReadAllLines(allText);
 
-            lines = lines
-                .Select((x, i) => $"{x}{lines.ElementAtOrDefault(i + 1)}")
-                .Where((_, i) => i % 2 == 0)
+            var parsedlines = lines                
                 .Select(s => 
                 (
                     date: _dateLine.MatchOr(_dateLine2, s), 
@@ -95,10 +109,12 @@ namespace ExportParser.Alfa
                     mcc: _descrLine.Match(s).Groups["mcc"].Value)
                 )
                 .Select(s => (date: TryDate(s.date), sum: TrySum(s.sum), s.addr, mcc: mccCode.TryGet(s.mcc)))
-                .Select(s => $"{s.date:dd/MM/yyyy};{Math.Abs(s.sum ?? 0)};{s.addr} - {s.mcc}")
+                .OrderBy(s => s.date)
+                .ThenBy(s => s.sum)
+                .Select(s => $"{s.date:dd/MM/yyyy};{Math.Abs(s.sum ?? 0)};{s.addr};{s.mcc}")
                 .ToArray();
 
-            File.WriteAllLines($"{Path.GetFileNameWithoutExtension(inputPdf.Name)}.csv", lines);
+            File.WriteAllLines(Path.Combine(inputPdf.DirectoryName, $"{Path.GetFileNameWithoutExtension(inputPdf.Name)}.csv"), parsedlines);
         }
     }
 }
